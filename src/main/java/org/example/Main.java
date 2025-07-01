c1c3c2d10d0550f89e1abaf6b255754b517b2628
Jadamaar07 <javierdavid742000@gmail.com>
2025-07-01 06:57:33 -0500

    Feat definir variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner keyEntry=new Scanner(System.in);
+
+        System.out.print("                         ▄▄         ▄▄                                                   ▄▄    ▄▄                             ▄▄          \n" +
+                "                         ██       ▀███                      ██                           ██  ▀███                           ▀███          \n" +
+                "                                    ██                      ██                                 ██                             ██          \n" +
+                "▀███▄███  ▄▄█▀██ ▄██▀██████    ▄█▀▀███   ▄▄█▀██▀████████▄ ██████      ▄▄█▀██▀██▀   ▀██▀▀███    ██      ▄██▀██  ▄██▀██▄   ▄█▀▀███   ▄▄█▀██ \n" +
+                "  ██▀ ▀▀ ▄█▀   ████   ▀▀ ██  ▄██    ██  ▄█▀   ██ ██    ██   ██       ▄█▀   ██ ██   ▄█    ██    ██     ██▀  ██ ██▀   ▀██▄██    ██  ▄█▀   ██\n" +
+                "  ██     ██▀▀▀▀▀▀▀█████▄ ██  ███    ██  ██▀▀▀▀▀▀ ██    ██   ██       ██▀▀▀▀▀▀  ██ ▄█     ██    ██     ██      ██     █████    ██  ██▀▀▀▀▀▀\n" +
+                "  ██     ██▄    ▄█▄   ██ ██  ▀██    ██  ██▄    ▄ ██    ██   ██       ██▄    ▄   ███      ██    ██     ██▄    ▄██▄   ▄██▀██    ██  ██▄    ▄\n" +
+                "▄████▄    ▀█████▀██████▀████▄ ▀████▀███▄ ▀█████▀████  ████▄ ▀████     ▀█████▀    █     ▄████▄▄████▄    █████▀  ▀█████▀  ▀████▀███▄ ▀█████▀\n" +
+                "                                                                                                                                          \n" +
+                "                                                                                                                                          \n");
+
+        //personajes Leon S Kennedy
+        //nombre
+        //edad
+        //cantidadVida
+
+        //personaje MR X (TITAN)
+        //nombre
+        //virus que lo infecto
+        //cantidad de vida
+
+        String personajes;
+        String nombre;
+        byte edad;
+        short cantidadVidaLeon;
+
+        String nombreMrX;
+        String virusInfecto;
+        short cantidadVidaTitan;
 
 
 
